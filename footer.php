a4d0f80ad8077cba3f8aadb5884cd602439d9bea
abhayshankur3812 <76917982+abhayshankur3812@users.noreply.github.com>
2021-09-16 15:27:25 +0530

    Revert "My E-commerce Website"
    This reverts commit 8e4280388594c76d4301031399a5661fca0a9f7b.

--- footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo "    <!-- Footer -->
-    <footer>
-        <h3><b>get IT </b> Copyright
-            <i class='material-icons' style='color: black; background: transparent;'>&#xe90c;</i> 2021
-        </h3>
-    </footer>";
-?>
